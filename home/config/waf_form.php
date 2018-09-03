525cde8f42edee6191d4cb772564e35d3dac83e1
alatcache <al@cache.co.uk>
2018-09-03 17:53:14 +0100

    More templates

--- home/config/waf_form.php
+++ home/config/waf_form.php
@@ -1,5 +1,7 @@
 <?php
 
 	return [
-		'input'=>'<input type="{{type}}" name="{{name}}" class="form-control input-small" {{attrs}} />',
+		'input'=>'<input type="{{type}}" name="{{name}}" class="form-control form-input form-{{type}}" {{attrs}} />',
+		'inputContainer' => '<div class="form-item form-type-input form-type-{{type}} {{type}}{{required}}">{{content}}</div>',
+		'select' => '<select name="{{name}}" class="form-control form-select" {{attrs}}>{{content}}</select>',
 	];
