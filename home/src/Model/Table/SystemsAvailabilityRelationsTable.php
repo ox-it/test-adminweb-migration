b6147d92656c9f892be07b39adfa0e0732b8d63d
alatcache <al@cache.co.uk>
2018-09-03 10:29:44 +0100

    Systems Availability table names

--- home/src/Model/Table/SystemsAvailabilityRelationsTable.php
+++ home/src/Model/Table/SystemsAvailabilityRelationsTable.php
@@ -10,29 +10,16 @@ class SystemsAvailabilityRelationsTable extends Table
 {
 
 	public static function defaultConnectionName() {
-			return 'systems_availability-test';
+		return 'systems_availability-test';
 	}
 
 	public function initialize(array $config)
 	{
-			$this->addBehavior('Timestamp');
-
-			$this->setTable('relations');
-			$this->table('relations');    			// Prior to 3.4.0
-
-			$this->belongsTo('SystemsAvailabilitySystems')->setForeignKey('system_id');
-			$this->belongsTo('SystemsAvailabilityViews')->setForeignKey('view_id');
+		$this->addBehavior('Timestamp');
+		$this->setTable('systems_availability_relations');
+		$this->setPrimaryKey('id');
+		$this->belongsTo('SystemsAvailabilitySystems')->setForeignKey('system_id');
+		$this->belongsTo('SystemsAvailabilityViews')->setForeignKey('view_id');
 	}
-
-	/*
-	public function getSystemsForViewID($viewID = null) {
-		$nowstamp = time();
-		$query = $this->find('all')
-			->where([ 'SystemsAvailabilityRelations.view_id' => $viewID ])
-			->order([ 'SystemsAvailabilityRelations.position' => 'ASC' ]);
-		$systems = $query->all();
-		return $systems;
-	}
-	//*/
 
 }
--- home/src/Model/Table/SystemsAvailabilityViewsTable.php
+++ home/src/Model/Table/SystemsAvailabilityViewsTable.php
@@ -16,10 +16,12 @@ class SystemsAvailabilityViewsTable extends Table
 	public function initialize(array $config)
 	{
 		$this->addBehavior('Timestamp');
-		$this->setTable('views');
+		$this->setTable('systems_availability_views');
+		$this->setPrimaryKey('id');
 		$this->belongsToMany('SystemsAvailabilitySystems',[
       'through' => 'SystemsAvailabilityRelations',
-      //'foreignKey' => 'view_id',
+      'joinTable' => 'systems_availability_relations',
+      'foreignKey' => 'view_id',
       'targetForeignKey' => 'system_id'
     ]);
 	}
@@ -29,78 +31,5 @@ class SystemsAvailabilityViewsTable extends Table
 		$views = $query->all();
 		return $views;
 	}
-
-
-    /*
-    public function getByID($eventID = null) {
-      $event = $this->get($eventID);
-
-      $event->waiting = ( $event->eventstatuscode=='F' || $event->category==4 );
-
-      $start_date = Time::createFromFormat('d/m/Y', $event->datestart);
-      $event->startstamp = $start_date->getTimestamp();
-
-      return $event;
-    }
-
-    public function futureEventsForCourseID($courseID = null) {
-      $events_query = $this->find('all', [
-        'conditions' =>
-					[
-						'SafetyEvents.courseID' => $courseID,
-						'SafetyEvents.eventstatuscode ' => 'O',
-						// Can't do this because they are stored as strings!
-						//'SafetyEvents.datestart >' => new Time('+12 hours'),
-					],
-				// Can't do this because they are stored as strings!
-				// 'order' => [ 'SafetyEvents.datestart' => 'ASC', 'SafetyEvents.timestart' => 'ASC' ]
-      ]);
-      $events = $events_query->all();
-      foreach ($events as $event) {
-        //print_r($event);
-        $start_date = Time::createFromFormat('d/m/Y H:i', $event->datestart . ' ' . $event->timestart);
-        $event->startstamp = $start_date->getTimestamp();
-        $event->month = date("m",$event->startstamp);
-        $cutoff_date = new Time('+12 hours');
-        $event->cutoff = $cutoff_date->getTimestamp();
-        $event->ignore = ($event->startstamp < $event->cutoff);
-        $open_date = Time::createFromFormat('d/m/Y', $event->bookingopen);
-        $event->openstamp = $open_date->getTimestamp();
-        if (!empty($event->bookingclosed)) {
-	        $closed_date = Time::createFromFormat('d/m/Y', $event->bookingclosed);
-	      } else {
-	        $closed_date = $cutoff_date;
-	      }
-        $event->closedstamp = $closed_date->getTimestamp();
-      }
-      $events->sortBy('startstamp');
-      $result = $events->reject(function ($event, $key) {
-        return $event->ignore === true;
-      });
-      return $result;
-    }
-
-    public function cancellableEvents() {
-      $nowstamp = time();
-      $query = $this->find('all')
-				->where([
-					'OR' => [['SafetyCourses.category' => 1], ['SafetyCourses.category' => 4]],
-					'OR' => [['eventstatuscode' => 'O'], ['eventstatuscode' => 'C'], ['eventstatuscode' => 'F']]
-			  ])
-        ->contain([ 'SafetyCourses' ]);
-      $events = $query->all();
-      foreach ($events as $event) {
-        $start_date = Time::createFromFormat('d/m/Y H:i', $event->datestart . ' ' . $event->timestart);
-        $event->startstamp = $start_date->getTimestamp();
-        $event->ignore = ($event->startstamp < $nowstamp);
-        //print_r($event);
-      }
-      $events->sortBy('startstamp');
-      $result = $events->reject(function ($event, $key) {
-        return $event->ignore === true;
-      });
-      return $result;
-    }
-    //*/
 
 }
