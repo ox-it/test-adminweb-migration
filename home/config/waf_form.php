7514ba8dc309b8c7e21777e88f524d0919062a09
alatcache <al@cache.co.uk>
2018-09-05 12:33:02 +0100

    More templates

--- home/config/waf_form.php
+++ home/config/waf_form.php
@@ -1,7 +1,9 @@
 <?php
 
 	return [
+	  'fieldset' => '<fieldset{{attrs}}><div class="panel-body">{{content}}</div></fieldset>',
 		'input'=>'<input type="{{type}}" name="{{name}}" class="form-control form-input form-{{type}} {{type}}" {{attrs}} />',
 		'inputContainer' => '<div class="webform-component form-item form-type-input form-type-{{type}} {{type}}{{required}}">{{content}}</div>',
+		'legend' => '<legend class="panel-heading">{{text}}</legend>',
 		'select' => '<select name="{{name}}" class="form-control form-select select" {{attrs}}>{{content}}</select>',
 	];
