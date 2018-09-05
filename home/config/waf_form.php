478f950ab4555e831d24b6be2e2793ffcebc6179
alatcache <al@cache.co.uk>
2018-09-05 12:12:59 +0100

    Webform class

--- home/config/waf_form.php
+++ home/config/waf_form.php
@@ -1,7 +1,7 @@
 <?php
 
 	return [
-		'input'=>'<input type="{{type}}" name="{{name}}" class="form-control form-input form-{{type}}" {{attrs}} />',
-		'inputContainer' => '<div class="form-item form-type-input form-type-{{type}} {{type}}{{required}}">{{content}}</div>',
-		'select' => '<select name="{{name}}" class="form-control form-select" {{attrs}}>{{content}}</select>',
+		'input'=>'<input type="{{type}}" name="{{name}}" class="form-control form-input form-{{type}} {{type}}" {{attrs}} />',
+		'inputContainer' => '<div class="webform-component form-item form-type-input form-type-{{type}} {{type}}{{required}}">{{content}}</div>',
+		'select' => '<select name="{{name}}" class="form-control form-select select" {{attrs}}>{{content}}</select>',
 	];
