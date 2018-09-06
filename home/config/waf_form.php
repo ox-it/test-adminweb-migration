1b2adec05f0920edaff51ce5a8800b6f823da35a
alatcache <al@cache.co.uk>
2018-09-06 09:54:15 +0100

    GCP

--- home/config/waf_form.php
+++ home/config/waf_form.php
@@ -1,6 +1,7 @@
 <?php
 
 	return [
+	  'button' => '<button{{attrs}} class="button btn">{{text}}</button>',
 	  'fieldset' => '<fieldset{{attrs}}><div class="panel-body">{{content}}</div></fieldset>',
 		'input'=>'<input type="{{type}}" name="{{name}}" class="form-control form-input form-{{type}} {{type}}" {{attrs}} />',
 		'inputContainer' => '<div class="webform-component form-item form-type-input form-type-{{type}} {{type}}{{required}}">{{content}}</div>',
