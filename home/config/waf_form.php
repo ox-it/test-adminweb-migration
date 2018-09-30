cf0f6a70bb3e311628fd4c127efabab6f0402e35
alatcache <al@cache.co.uk>
2018-09-30 08:29:56 +0100

    Radio Checkbox wrappers

--- home/config/waf_form.php
+++ home/config/waf_form.php
@@ -2,11 +2,14 @@
 
 	return [
 	  'button' => '<button{{attrs}} class="button btn">{{text}}</button>',
+	  'checkboxWrapper' => '<div class="form-item form-item-submitted-checkbox-{{name}} form-type-checkbox checkbox">{{label}}</div>',
 	  'fieldset' => '<fieldset{{attrs}}><div class="panel-body">{{content}}</div></fieldset>',
 		'input'=>'<input type="{{type}}" name="{{name}}" class="form-control form-input form-{{type}} {{type}}" {{attrs}} />',
 		'inputContainer' => '<div class="webform-component form-item form-type-input form-type-{{type}} {{type}}{{required}}">{{content}}</div>',
 		'inputContainerError' => '<div class="webform-component form-item form-type-input form-type-{{type}} {{type}}{{required}} error">{{content}}{{error}}</div>',
 		'legend' => '<legend class="panel-heading">{{text}}</legend>',
 		'select' => '<select name="{{name}}" class="form-control form-select select" {{attrs}}>{{content}}</select>',
+		'radio' => '<input type="radio" name="{{name}}" value="{{value}}"{{attrs}}>',
+    'radioWrapper' => '<div class="form-item form-item-submitted-checkbox-{{name}} form-type-checkbox checkbox">{{label}}</div>',
     'textarea' => '<textarea class="form-control form-textarea" name="{{name}}"{{attrs}}>{{value}}</textarea>',
 	];
