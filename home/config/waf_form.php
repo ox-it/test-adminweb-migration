20cd3dfe86815987e8046563ca76e2ee4f8611d5
alatcache <al@cache.co.uk>
2018-09-30 10:45:58 +0100

    Form templates

--- home/config/waf_form.php
+++ home/config/waf_form.php
@@ -9,8 +9,9 @@
 		'inputContainerError' => '<div class="webform-component form-item form-type-input form-type-{{type}} {{type}}{{required}} error">{{content}}{{error}}</div>',
 		'label' => '<label class="control-label" {{attrs}}>{{text}}</label>',
 		'legend' => '<legend class="panel-heading">{{text}}</legend>',
+		'nestingLabel' => '{{hidden}}<label class="control-label"{{attrs}}>{{input}}{{text}}</label>',
 		'select' => '<select name="{{name}}" class="form-control form-select select" {{attrs}}>{{content}}</select>',
 		'radio' => '<input class="form-radio" type="radio" name="{{name}}" value="{{value}}"{{attrs}}>',
-    'radioWrapper' => '<div class="form-item form-item-submitted-radio form-type-radio radio">{{label}}</div>',
+    'radioWrapper' => '<div class="form-item form-type-radio radio">{{label}}</div>',
     'textarea' => '<textarea class="form-control form-textarea" name="{{name}}"{{attrs}}>{{value}}</textarea>',
 	];
