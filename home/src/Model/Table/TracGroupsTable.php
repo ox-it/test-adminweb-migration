2786c90dc2f6983c4365aae0cf8b63c93bec21a8
alatcache <al@cache.co.uk>
2019-05-27 13:56:46 +0100

    Trac database updates

--- home/src/Model/Table/TracGroupsTable.php
+++ home/src/Model/Table/TracGroupsTable.php
@@ -23,7 +23,7 @@ class TracGroupsTable extends Table
     $db_config = $config['connection']->config();
     $prefix = empty($db_config['prefix']) ? '' : $db_config['prefix'];
     $table = $prefix . 'group';
-    if (!empty($db_config['tables']) && is_array($db_config['tables']) && count($db_config['tables'])==2) $table = str_replace($table, $db_config['prefix'][0], $db_config['prefix'][1]);
+    if (!empty($db_config['tables']) && is_array($db_config['tables']) && count($db_config['tables'])==2) $table = str_replace($db_config['prefix'][0], $db_config['prefix'][1], $table);
     $this->addBehavior('Timestamp');
     $this->setTable($table);
     $this->setPrimaryKey('weekly_group');
